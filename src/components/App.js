e0ca395463053a3cef6cc2c15468afbffa4c8a39
shivangbhardwaj2001 <shivangbhardwaj2001@gmail.com>
2023-02-24 23:52:21 +0530

    pushing

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+  const text = "Newton School"
+  return (
+    <div id="main">
+    </div>
+  )
+}
+
+
+export default App;
