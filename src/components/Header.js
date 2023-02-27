e0a92f1847d3f6998c16532f76e9eff150695e4b
Shivang Bhardwaj <84633238+shivangbhardwaj2001@users.noreply.github.com>
2023-02-28 03:26:32 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    const text = "Newton School"
+    return (
+        <div id="header-component">
+          
+        </div>
+    )
+}
+
+
+export default Header;
